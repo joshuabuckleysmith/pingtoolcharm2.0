fb1e00266181744aa3b560e7428648a14ccf28f5
Joshua Smith (CW) <joshuabuckleysmith@gmail.com>
2018-01-25 12:46:58 -0800

    pre-UX

--- app/tkwindows.py
+++ app/tkwindows.py
@@ -1,6 +1,7 @@
 import collections
 import os
 import tkinter as tk
+from tkinter import ttk
 from time import sleep
 from app import pingcomponents
 from app import killthread
@@ -10,7 +11,8 @@ from app import logger
 from app import outputboxprinter
 from app import pingstatsgen
 
-
+style = ttk.Style()
+style.theme_use('clam')
 outstats = pingstatsgen.getstats
 wlog=logger.log.writelogline
 rlog=logger.log.readlog
@@ -21,8 +23,9 @@ outlogset = outputboxprinter.outlog.set
 outboxclear = outputboxprinter.outbox.clear
 outlogclear = outputboxprinter.outlog.clear
 root = tk.Tk()
-starticon = tk.PhotoImage(file="start2.png")
-cancelicon = tk.PhotoImage(file="cancel.png")
+startreleasedicon = tk.PhotoImage(file="startenabled.png")
+startpressedicon = tk.PhotoImage(file="startdepressed.png")
+cancelreleasedicon = tk.PhotoImage(file="cancel.png")
 test = tk.StringVar()
 pingnumber = tk.IntVar()
 mtu = tk.StringVar()
@@ -30,19 +33,25 @@ hmtu = tk.StringVar()
 store = tk.StringVar()
 prefix = tk.StringVar()
 mturadio = tk.StringVar()
-prefix.set("IP Address")
+prefix.set("Router(dg)")
 logoutput = tk.StringVar()
 storetxt = tk.Label(text="Store Number")
 wlog("vars set")
 arial = ("Arial", "10")
 consolas = ("Consolas", 8)
 arial = consolas
+bgcolor = "#000000"
+fgcolor = "#EEEEEE"
 options = collections.OrderedDict(
     [
-        ("IP Address", ""), ("Router(dg)", "dg"),
+        ("Router(dg)", "dg"),
         ("Switch US(ussw010)", "ussw010"),
         ("Switch Canada(casw010)", "casw010"),
-        ("Workstation(mws)", "mws"), ("FoH Switch(ussw030)", "ussw030")
+        ("Register US(usrg010)", "usrg010"),
+        ("Register Canada(carg010)", "carg010"),
+        ("Workstation(mws)", "mws"),
+        ("FoH Switch(ussw030)", "ussw030"),
+        ("IP Address", "")
     ]
 )
 
@@ -70,36 +79,32 @@ def destroyapp():
 def buttons():
     wlog("buttons from tkwindows")
     '''creates tk window'''
-    storetxt = tk.Label(text="Store Number", font = arial)  # Ping test labels for text boxes.
+    storetxt = ttk.Label(text="Store Number", font = arial, bg=bgcolor, fg=fgcolor)  # Ping test labels for text boxes.
     storetxt.place(x=10, y=10)
-    pingtxt = tk.Label(text="Ping Number", font = arial)  # Ping test labels for text boxes.
+    pingtxt = tk.Label(text="Ping Number", font = arial, bg=bgcolor, fg=fgcolor)  # Ping test labels for text boxes.
     pingtxt.place(x=150, y=10)
-    mtutxt = tk.Label(text="Default MTU", font = arial)  # Ping test labels for text boxes.
-    mtutxt.place(x=31, y=100)
-    hmtutxt = tk.Label(text="Alternate MTU", font = arial)  # Ping test labels for text boxes.
-    hmtutxt.place(x=171, y=100)
-    storeentry = tk.Entry(root, textvariable=store, font = consolas) # Entry box for store number for ip address
+    mtutxt = tk.Label(text="Default MTU", font = arial, bg=bgcolor, fg=fgcolor)  # Ping test labels for text boxes.
+    mtutxt.place(x=30, y=94)
+    hmtutxt = tk.Label(text="Alternate MTU", font = arial, bg=bgcolor, fg=fgcolor)  # Ping test labels for text boxes.
+    hmtutxt.place(x=170, y=94)
+    storeentry = tk.Entry(root, textvariable=store, font = consolas, bg=bgcolor, fg=fgcolor) # Entry box for store number for ip address
     storeentry.place(x=10, y=30)
-    pingentry = tk.Entry(textvariable=pingnumber, font = consolas) # Entry box for ping number
+    pingentry = tk.Entry(textvariable=pingnumber, font = consolas, bg=bgcolor, fg=fgcolor) # Entry box for ping number
     pingentry.place(x=150, y=30)
-    mtuentry = tk.Entry(textvariable=mtu, font = consolas) # Entry box for mtu
-    mtuentry.place(x=10, y=125)
+    mtuentry = tk.Entry(textvariable=mtu, font = consolas, bg=bgcolor, fg=fgcolor) # Entry box for mtu
+    mtuentry.place(x=10, y=115)
     mtuentry.insert(tk.END, "1345")
-    hmtuentry = tk.Entry(textvariable=hmtu, font = consolas) # Entry box for high mtu
-    hmtuentry.place(x=150, y=125)
+    hmtuentry = tk.Entry(textvariable=hmtu, font = consolas, bg=bgcolor, fg=fgcolor) # Entry box for high mtu
+    hmtuentry.place(x=150, y=115)
     hmtuentry.insert(tk.END, "4000")
     dropout = tk.OptionMenu(root, prefix, *options) # dropdown for ping choices
-    dropout.config(font=arial)
-    dropout.nametowidget(dropout.menuname).config(font=arial)
+    dropout.config(font=arial, bg=bgcolor, fg=fgcolor)
+    dropout.nametowidget(dropout.menuname).config(font=arial, bg=bgcolor, fg=fgcolor)
     dropout.place(x=7, y=50)
-    testbutton = tk.Checkbutton\
-        (text="Use Alternate Mtu", font = arial, variable=test, onvalue="secondary",
-        offvalue="primary", )                           # dropdown for mtu choices
-    testbutton.deselect()
-    mturadiobutton = tk.Radiobutton(text="", variable=mturadio, value="primary")
-    mturadiobutton2 = tk.Radiobutton(text="", variable=mturadio, value="secondary")
-    mturadiobutton.place(x=6, y=100)
-    mturadiobutton2.place(x=146, y=100)
+    mturadiobutton = tk.Radiobutton(text="", border=0, variable=mturadio, value="primary", bg=bgcolor, fg=fgcolor)
+    mturadiobutton2 = tk.Radiobutton(text="", border=0, variable=mturadio, value="secondary", bg=bgcolor, fg=fgcolor)
+    mturadiobutton.place(x=9, y=94)
+    mturadiobutton2.place(x=149, y=94)
     mturadiobutton.select()
     #testbutton.place(x=80, y=150)
 
@@ -112,13 +117,15 @@ def buttons():
         outboxclear()
         outlogclear()
         sp.sp(store.get(), mturadio.get(), pingnumber.get(), ping, cancelping, prefix.get(), options, storetxt, mtu.get(), hmtu.get())
-    ping = tk.Button(image = starticon,  relief=tk.SUNKEN, command=spf, border = "0") # Button runs SPF
-    ping.place(x=20, y=180)
+
+
+    ping = tk.Button(image=startreleasedicon, relief = tk.SUNKEN, border="0", command=spf, bg=bgcolor, fg=fgcolor)  # Button runs SPF
+    ping.place(x=0, y=130)
 
     def buttpress(*args):
-        ping.config(image=starticon)
+        ping.config(image=startpressedicon)
     def buttrelease(*args):
-        ping.config(image=cancelicon)
+        ping.config(image=startreleasedicon)
 
     ping.bind("<ButtonPress>", buttpress)
     ping.bind("<ButtonRelease>", buttrelease)
@@ -128,23 +135,23 @@ def buttons():
     def killthreadf():
         wlog("killthreadf run")
         killthread.killthread(cancelping, ping)
-    cancelping = tk.Button(image = cancelicon, command=killthreadf, border = "2") #Cancel button
+    cancelping = tk.Button(image = cancelreleasedicon, command=killthreadf, border ="2") #Cancel button
     cancelping['state'] = 'disabled'
     #cancelping.place(x=160, y=180)
 
 
     #===============================================================
     wlog("loggingwindow run")
-    statsoutputbox = tk.Text(root, height=5, width=65, font="Consolas 8")
+    statsoutputbox = tk.Text(root, height=5, width=65, font="Consolas 8", bg=bgcolor)
     statsoutputbox.place(x=300, y=15)
     statsoutputbox.see("end")
     outputboxx = 300
     outputboxy = 100
-    outputbox = tk.Text(root, height=14, width=65, font="Consolas 8")
+    outputbox = tk.Text(root, height=10, width=65, font="Consolas 8", bg=bgcolor)
     outputbox.place(x=300, y=100)
     outputbox.see("end")
-    scrol2 = tk.Scrollbar(root, command=outputbox.yview, )
-    scrol2.place(x=outputboxx+378, y=outputboxy, height=186)
+    scrol2 = tk.Scrollbar(root, command=outputbox.yview, bg=bgcolor)
+    scrol2.place(x=outputboxx+378, y=outputboxy, height=134)
 
 
     #========================================outputs realtime ping data
@@ -185,7 +192,7 @@ def buttons():
     loggingwindow = tk.Text(logout, height=40, width=80, font="Consolas 10")
     loggingwindow.grid(row=0, column=0)
     loggingwindow.see("end")
-    scrollb = tk.Scrollbar(logout, command=loggingwindow.yview)
+    scrollb = tk.Scrollbar(logout, command=loggingwindow.yview, bg=bgcolor)
     loggingwindow['yscrollcommand'] = scrollb.set
     scrollb.grid(row=0, column=1, sticky='nsew')
 
@@ -245,5 +252,6 @@ def buttons():
     textdaemon.setDaemon(True)
     textdaemon.start()
     root.resizable(width=False, height=False)
-    root.geometry('{}x{}'.format(710,310))
+    root.geometry('{}x{}'.format(710,260))
+    root.configure(bg=bgcolor)
     root.mainloop()
---
+++ ttktest.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+store = tk.StringVar()
+store.set("4444")
+#store = "4444"
+style = ttk.Style()
+#style.theme_use('clam')
+
+scrol2 = ttk.Scrollbar(root)
+scrol2.place(x=1, y=2, height=134)
+storeentry = tk.Entry(root, textvariable=store) # Entry box for store number for ip address
+storeentry.place(x=10, y=30)
+
+root.mainloop()
